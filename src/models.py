037ffc7ef57f5c5b1b3cc4087eabc4f15b6c6763
RubenGonzalez1983 <gonzalezjuarez83@gmail.com>
2021-08-21 16:06:01 +0000

    firstcommit

--- src/models.py
+++ src/models.py
@@ -17,3 +17,85 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    birth_year = db.Column(db.String(120))
+    eye_color = db.Column(db.String(120))
+    hair_color = db.Column(db.String(120))
+    homeworld_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    homeworld = db.relationship("Planets")
+    gender = db.Column(db.String(120))
+    height = db.Column(db.String(120))
+    mass = db.Column(db.String(120))
+    skin_color = db.Column(db.String(120))
+    url = db.Column(db.String(120))
+    created = db.Column(db.String(120))
+    edited = db.Column(db.String(120))
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            # do not serialize the password, its a security breach
+        }
+    
+
+class Vehicle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    model = db.Column(db.String(120))
+    vehicle_class = db.Column(db.String(120))
+    manufacturer = db.Column(db.String(120))
+    length = db.Column(db.String(120))
+    cost_in_credits = db.Column(db.String(120))
+    crew = db.Column(db.String(120))
+    passengers = db.Column(db.String(120))
+    max_atmosphering_speed = db.Column(db.String(120))
+    cargo_capacity = db.Column(db.String(120))
+    consumables = db.Column(db.String(120))
+    url = db.Column(db.String(120))
+    created = db.Column(db.String(120))
+    edited = db.Column(db.String(120))
+
+
+    def __repr__(self):
+        return '<Vehicle %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            # do not serialize the password, its a security breach
+        }
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    diameter = db.Column(db.String(120))
+    rotation_period = db.Column(db.String(120))
+    orbital_period = db.Column(db.String(120))
+    gravity = db.Column(db.String(120))
+    population = db.Column(db.String(120))
+    climate = db.Column(db.String(120))
+    terrain = db.Column(db.String(120))
+    surface_water = db.Column(db.String(120))
+    url = db.Column(db.String(120))
+    created = db.Column(db.String(120))
+    edited = db.Column(db.String(120))
+
+    def __repr__(self):
+        return '<Vehicle %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            # do not serialize the password, its a security breach
+        }
+
+   
